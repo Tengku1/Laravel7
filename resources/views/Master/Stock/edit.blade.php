55bb5535cd88c7a71f949a0908461e304599c04c
Tengku1 <anountengku@gmail.com>
2020-07-07 12:11:01 +0800

    Penambahan Layouts khusus untuk Master

---
+++ resources/views/Master/Stock/edit.blade.php
@@ -0,0 +1,64 @@
+@extends('Admin.layout.master',['title'=>'Edit - Laravel 7'])
+
+@section('content')
+<div class="card">
+    <div class="card-header">
+        Add Data
+    </div>
+    <div class="card-body">
+        <form action="/stock/{{$stocks['id']}}/update" method="post">
+            @method('patch')
+            @csrf
+            <input type="hidden" name="id" value="{{$stocks['id']}}">
+            <div class="form-group row">
+                <div class="col-md-6 px-1">
+                    <label for="name" class="font-weight-bold">Product Name</label>
+                    <input type="text" class="form-control" name="name" id="name" placeholder="Product Name ..."
+                        value="{{ old('title') ?? $stocks['name']}}">
+                    @error('name')
+                    <span class="text-danger">{{$message}}</span>
+                    @enderror
+                </div>
+                <div class="col-md-6 px-1">
+                    <label for="code">Branch Code</label>
+                    <select name="code" id="code" class="custom-select">
+                        @foreach ($stocks['branch'] as $item)
+                        <option value="{{$item['code']}}">{{strval($item['name'])}}</option>
+                        @endforeach
+                    </select>
+                    @error('code')
+                    <span class="text-danger">{{$message}}</span>
+                    @enderror
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-md-6 px-1">
+                    <label for="sell_price" class="font-weight-bold">Sell Price</label>
+                    <input type="text" class="form-control" name="sell_price" id="sell_price"
+                        placeholder="Product Name ..." value="{{ old('sell_price') ?? $stocks['sell_price']}}">
+                    @error('sell_price')
+                    <span class="text-danger">{{$message}}</span>
+                    @enderror
+                </div>
+                <div class="col-md-6 px-1">
+                    <label for="buyprice" class="font-weight-bold">Buy Price</label>
+                    <input type="text" class="form-control" name="buyprice" id="buyprice" placeholder="Product Name ..."
+                        value="{{ old('buy_price') ?? $stocks['buy_price']}}">
+                    @error('buyprice')
+                    <span class="text-danger">{{$message}}</span>
+                    @enderror
+                </div>
+
+            </div>
+            <div class="col-md-12 btn-group mt-3">
+                <button type="submit" class="btn btn-primary">Update <i class="fa fa-pencil-square-o"></i></button>
+                <button type="reset" class="btn btn-danger">Clear <i class="fa fa-trash"></i></button>
+                <a href="{{URL::previous()}}">
+                    <button type="button" class="btn btn-primary rounded-0">Back</button>
+                </a>
+            </div>
+        </form>
+    </div>
+</div>
+
+@endsection
---
+++ resources/views/Master/Stock/show.blade.php
@@ -0,0 +1,61 @@
+@extends('Admin.layout.master',['title'=>'Show Data - Laravel 7'])
+
+@section('content')
+<div class="card">
+    <div class="card-header">
+        Detail Data
+    </div>
+
+    <div class="card-body">
+        Added From {{$product['created_at']->diffForHumans()}}
+        @if ($product['qty'] < 20 && $product['qty'] > 0)
+            <div class="row mt-3 alert alert-warning">
+                The Product Running Low ! Increase Now !
+            </div>
+        @elseif ($product['qty'] <= 0)
+            <div class="row mt-3 alert alert-danger">
+                Out Of Product ! Add Now !
+            </div>
+        @endif
+        <div class="table-responsive">
+            <table class="col-md-12 mt-3 table table-hover table-light">
+                <tr>
+                    <td>ID</td>
+                    <td>:</td>
+                    <td>{{$product['id']}}</td>
+                </tr>
+                <tr>
+                    <td>Title</td>
+                    <td>:</td>
+                    <td>{{$product['name']}}</td>
+                </tr>
+                <tr>
+                    <td>Slug</td>
+                    <td>:</td>
+                    <td>{{$product['slug']}}</td>
+                </tr>
+                <tr>
+                    <td>Quantity</td>
+                    <td>:</td>
+                    <td>{{$product['qty']}}</td>
+                </tr>
+                <tr>
+                    <td>Created At</td>
+                    <td>:</td>
+                    <td>{{$product['created_at']}}</td>
+                </tr>
+                <tr>
+                    <td>Updated At</td>
+                    <td>:</td>
+                    <td>{{$product['modified_at']}}</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <div class="card-footer">
+        <a href="{{URL::previous()}}">
+            <button class="btn btn-primary">Back</button>
+        </a>
+    </div>
+</div>
+@endsection
