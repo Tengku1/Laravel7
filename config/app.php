2ecb38734a0ef6de93cf05d0c1d28836fa79fea7
Tengku1 <anountengku@gmail.com>
2020-07-15 13:00:22 +0800

    Small Changes

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://joli-chocolatine-82300.herokuapp.com/stock/branch/1'),
 
     'asset_url' => env('ASSET_URL', null),
 
