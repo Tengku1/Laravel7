ce979c8cef23b5f18b3af436f2036cef438a461c
Tengku1 <anountengku@gmail.com>
2020-07-16 09:40:33 +0800

    Litle changes

--- resources/views/layouts/Navbar.blade.php
+++ resources/views/layouts/Navbar.blade.php
@@ -1,6 +1,6 @@
 <header>
     <div class="col-md-10 float-left text-center pt-3">
-        <h4><a href="/">Crux</a></h4>
+        <h4 class="ml-5"><a href="/">Crux</a></h4>
     </div>
     <div class="col-md-1 float-left text-right pr-4 pt-2">
         @guest
