9d18d388452ecd2b9391260db5e8222ab4d3ec5f
Tengku1 <anountengku@gmail.com>
2020-07-19 22:15:07 +0800

    Perubahan

--- resources/views/Master/Product/index.blade.php
+++ resources/views/Master/Product/index.blade.php
@@ -66,7 +66,40 @@ $no = 1;
 </div>
 
 <!-- Modal -->
-@include('layouts.modalDelete')
+<div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form action="/product/destroy" method="post">
+                {{method_field('delete')}}
+                {{ csrf_field() }}
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Delete Product</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    You Want You Sure Delete This Record?
+                    <input type="hidden" name="id" id="id">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-danger waves-effect remove-data-from-delete-form">Delete</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@section('script')
+
+<script>
+    $(document).on('click', '.delete', function () {
+        var id = $(this).attr('data-id');
+        $('#id').val(id);
+    });
+</script>
+
+@endsection
 {{-- End Modal --}}
 
 {{-- For Pagination --}}
