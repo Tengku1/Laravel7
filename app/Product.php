d57244ce93637c2989bc2338ab738c125932ca38
Tengku1 <anountengku@gmail.com>
2020-07-28 22:28:43 +0800

    Strange

--- app/Product.php
+++ app/Product.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = ['name', 'slug', 'sell_price', 'status'];
+    public $timestamp = false;
+    const UPDATED_AT = "modified_at";
+    const CREATED_AT = "created_at";
     public function Stock()
     {
         return $this->hasMany(Products_Stock::class);
